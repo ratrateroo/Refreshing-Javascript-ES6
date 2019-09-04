d53b63297491bf301bd058a3877a8c9a108c46d9
Mark Leo <ratrateroo@gmail.com>
2019-09-05 03:52:32 +0800

    tried to fix some bugs

---
+++ js/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var ELEMENTS = _interopRequireWildcard(require("elements.js"));
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
+
+ELEMENTS.ELEMENT_SEARCHED_BUTTON.addEventListener('click', searchWeather);
+
+function searchWeather() {
+  alert('Clicked');
+}
---
+++ src/app.js
@@ -0,0 +1,7 @@
+import * as ELEMENTS from './elements';
+
+ELEMENTS.ELEMENT_SEARCHED_BUTTON.addEventListener('click', searchWeather);
+
+function searchWeather(){
+    alert('Clicked');
+}
