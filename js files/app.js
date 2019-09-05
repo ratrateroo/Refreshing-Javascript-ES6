31474795d8bfde61817097edda31396c44b4d138
Mark Leo <ratrateroo@gmail.com>
2019-09-05 22:00:17 +0800

    added webpack for es6 codes

--- js files/app.js
+++ js files/app.js
@@ -3,5 +3,6 @@ import * as ELEMENTS from './elements';
 ELEMENTS.ELEMENT_SEARCHED_BUTTON.addEventListener('click', searchWeather);
 
 function searchWeather(){
+    const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.nodeValue;
     alert('Clicked');
 }
--- js/app.js
+++ js/app.js
@@ -1,11 +1,12 @@
 "use strict";
 
-var ELEMENTS = _interopRequireWildcard(require("elements.js"));
+var ELEMENTS = _interopRequireWildcard(require("./elements"));
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) { var desc = Object.defineProperty && Object.getOwnPropertyDescriptor ? Object.getOwnPropertyDescriptor(obj, key) : {}; if (desc.get || desc.set) { Object.defineProperty(newObj, key, desc); } else { newObj[key] = obj[key]; } } } } newObj.default = obj; return newObj; } }
 
 ELEMENTS.ELEMENT_SEARCHED_BUTTON.addEventListener('click', searchWeather);
 
 function searchWeather() {
+  const CITY_NAME = ELEMENTS.ELEMENT_SEARCHED_CITY.nodeValue;
   alert('Clicked');
 }
